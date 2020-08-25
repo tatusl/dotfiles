62544b548e56053122b81054c638eec79eb097a1
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2020-08-25 14:12:57 +0300

    [zsh] Add completions for azure-cli

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -4,3 +4,9 @@ if [ $(date +'%j') != $(stat -f '%Sm' -t '%j' ~/.zcompdump) ]; then
 else
   compinit -C
 fi
+
+# Load azure-cli completions and enable bash completions in zsh
+if [ -f "/usr/local/etc/bash_completion.d/az" ]; then
+  autoload -U +X bashcompinit && bashcompinit
+  source /usr/local/etc/bash_completion.d/az
+fi
