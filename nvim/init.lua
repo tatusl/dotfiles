b37e7fa994992531b811ac0fdd5da1520844210e
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2022-01-13 23:25:06 +0200

    [nvim] Add initial basic configuration

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+--[[
+Highly inspired and shamelessly copypasted from
+https://github.com/LunarVim/Neovim-from-scratch
+--]]
+
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.lualine"
