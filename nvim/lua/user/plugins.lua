6495e884bc93c97e1985503b54a270b9a93fe4f2
Tatu Seppä-Lassila <tatu@seppa-lassila.fi>
2022-01-14 22:21:34 +0200

    Completions with nvim-cmp

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -50,6 +50,15 @@ return packer.startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
